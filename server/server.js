b44b356d823828f22a1f09d1d74428932d1258c5
Eduardo Model <edu_model@hotmail.com>
2018-06-24 17:40:05 -0300

    Adicionados os eventos de connect e disconnect

--- server/server.js
+++ server/server.js
@@ -1,19 +1,34 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
+
+//Esta forma de acessar o diretório public é ruim, pois tu acessa a pasta server, sai dela e entra na pasta public!
+// console.log(__dirname + '/../public')
+// console.log(publicPath)
+
 
 
 //Desta forma, tu utiliza uma foo integrada ao node que elemina o fato de ter que descer até um diretório e voltar!
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 let app = express()
+let server = http.createServer(app)
+//cria o web socket
+let io = socketIO(server)
 
 app.use(express.static(publicPath))
 
+//evento onde um usuário conecta ao servidor. Utiliza conexões persistentes entre os dois!
+io.on('connection', (socket) => {
+	console.log('Novo usuário conectado!')
+	socket.on('disconnect', () => {
+		console.log('Usuário desconectado!')
+	})
+})
 
-//Esta forma de acessar o diretório public é ruim, pois tu acessa a pasta server, sai dela e entra na pasta public!
-// console.log(__dirname + '/../public')
-// console.log(publicPath)
 
-app.listen(port, () => {
+
+server.listen(port, () => {
 	console.log(`Server iniciado na porta ${port}`)
 })
