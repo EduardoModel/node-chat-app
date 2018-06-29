094d53d3884b32a7c84b9bd48064f69b3ab837c7
Eduardo Model <edu_model@hotmail.com>
2018-06-29 18:11:58 -0300

    Adicionado formulário para mensagens e mostrando mensagens no browser

--- server/server.js
+++ server/server.js
@@ -30,10 +30,10 @@ io.on('connection', (socket) => {
 	socket.broadcast.emit('newMessage', generateMessage('Admin', 'Um novo usuário juntou-se a conversa.'))
 
 
-	socket.on('createMessage', (message) => {
+	socket.on('createMessage', (message, callback) => {
 		console.log('Nova mensagem!', message)
 		io.emit('newMessage', generateMessage(message.from, message.text))
-
+		callback('Isso é do server');
 		//envia a mensagem para todos, menos para quem gerou o evento
 		// socket.broadcast.emit('newMessage',  {
 		// 	from: message.from,
